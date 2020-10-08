f0810f603cdc34616b9470b83bac33a694c6c52e
Lisias <me@lisias.net>
2020-10-08 18:36:55 -0300

    Versioning and Automation scripts

--- Source/RecoveryController/Properties/AssemblyInfo.cs
+++ Source/RecoveryController/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.0.3.8")]
+[assembly: AssemblyVersion(RecoveryController.Version.Number)]
 [assembly: AssemblyFileVersion("1.0.0.0")]
